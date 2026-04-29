a30b165f692e55974e7e111b3d7fc5ddbbe19dfe
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-04-29 20:46:10 +0600

    refactor: rename index component to Home and update text content

--- app/(tab)/index.tsx
+++ app/(tab)/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export default function index() {
+export default function Home() {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Home</Text>
     </View>
   );
 }
